1a8ece9f9e60941c12cb36dc382ca421c6c906ef
haidoan4632 <doanhai01287181543@gmail.com>
2023-02-22 10:31:47 +0700

    first commit

--- src/ss2_loop/bai_2/Average.java
+++ src/ss2_loop/bai_2/Average.java
@@ -10,8 +10,28 @@ public class Average {
         System.out.println("Input number2: ");
         int number2 = Integer.parseInt(scanner.nextLine());
         int average;
-        int min;
-        int max;
-        
+        int uocChungMax = 1;
+        int boiChungMin;
+        if (number1 >= number2) {
+            for (int i = 1; i < number1; i++) {
+                if (number1 % i == 0 && number2 % i == 0) {
+                    uocChungMax = i;
+                }
+            }
+        } else {
+            for (int i = 1; i < number2; i++) {
+                if (number1 % i == 0 && number2 % i == 0) {
+                    uocChungMax = i;
+                }
+            }
+        }
+        System.out.println("Ước chung lớn nhất của 2 số là: ");
+        System.out.println(uocChungMax);
+        boiChungMin = number1 * number2 / uocChungMax;
+        System.out.println("Bội chung nhỏ nhất của 2 số là: ");
+        System.out.println(boiChungMin);
+        average = (uocChungMax + boiChungMin) / 2;
+        System.out.println("Trung bình cộng của UCLN và BCNN là: ");
+        System.out.println(average);
     }
 }
