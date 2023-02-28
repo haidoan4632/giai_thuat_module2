0d16a94306cb8bf73363f1a6c348c8ba3c6ef467
haidoan4632 <doanhai01287181543@gmail.com>
2023-02-28 20:11:18 +0700

    first commit

--- src/ss8_clean_code/CleanCode.java
+++ src/ss8_clean_code/CleanCode.java
@@ -1,10 +1,18 @@
 package ss8_clean_code;
-
     public class CleanCode {
+        public int sum(int num1, int num2, int num3) {
+            int sum = num1 + num2 + num3;
+            return sum;
+        }
+
+        private void employeeDetails(String name, String age, String awards, String ctc, String experience) {
+            System.out.println("{" + name + " " + age + " " + awards + " " + ctc + " " + experience + "}");
+        }
 
         public boolean isLeapYear(int year) {
             return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
         }
+
         public int getDayOfMonth(int month, int year) {
             switch (month) {
                 case 1:
