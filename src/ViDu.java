fd44d9f0be0c4a4043566f22826c2440569c09c0
haidoan4632 <doanhai01287181543@gmail.com>
2023-03-17 09:05:28 +0700

    first commit

---
+++ src/ViDu.java
@@ -0,0 +1,10 @@
+public class ViDu {
+    public static void main(String[] args) {
+        String thongBao = "Xin chào";
+        String hoVaTen = "Nguyễn Đắc Quốc Cường";
+        String tenSach = "Lap trinh java";
+        System.out.println("Thông báo: " + thongBao);
+        System.out.println("Họ và tên: " + hoVaTen);
+        System.out.println("Tên sách: " + tenSach);
+    }
+}
